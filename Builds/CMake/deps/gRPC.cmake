24f453b3bae1c37a2148bd88390597bd8c18eba2
gregtatcam <gtsipenyuk@ripple.com>
2020-05-11 18:03:58 -0400

    Add find package for gRPC

--- Builds/CMake/deps/gRPC.cmake
+++ Builds/CMake/deps/gRPC.cmake
@@ -7,64 +7,135 @@ else ()
   set (grpc_suffix "_unsecure")
 endif ()
 
-#add_executable (gRPC::grpc_cpp_plugin IMPORTED)
-#exclude_if_included (gRPC::grpc_cpp_plugin)
+find_package (gRPC 1.23 CONFIG QUIET)
+if (TARGET gRPC::gpr AND NOT local_grpc)
+  get_target_property (_grpc_l gRPC::gpr IMPORTED_LOCATION_DEBUG)
+  if (NOT _grpc_l)
+    get_target_property (_grpc_l gRPC::gpr IMPORTED_LOCATION_RELEASE)
+  endif ()
+  if (NOT _grpc_l)
+    get_target_property (_grpc_l gRPC::gpr IMPORTED_LOCATION)
+  endif ()
+  message (STATUS "Found cmake config for gRPC. Using ${_grpc_l}.")
+else ()
+  find_package (PkgConfig QUIET)
+  if (PKG_CONFIG_FOUND)
+    pkg_check_modules (grpc QUIET "grpc${grpc_suffix}>=1.25" "grpc++${grpc_suffix}" gpr)
+  endif ()
 
-# AbseilHelpers.cmake build type check checks _build_type for 
-# "static". this appers to conflict with static set in 
-# RippledSettings.cmake
-set(static_bck ${static})
-unset(static CACHE)
+  if (grpc_FOUND)
+    message (STATUS "Found gRPC using pkg-config. Using ${grpc_gpr_PREFIX}.")
+  endif ()
 
-FetchContent_GetProperties(grpc_src)
-FetchContent_Populate(
-  grpc_src 
-  QUIET
-  GIT_REPOSITORY https://github.com/grpc/grpc.git
-  GIT_TAG v1.28.0
-  SOURCE_DIR ${nih_cache_path}/src/grpc_src
-  BINARY_DIR ${nih_cache_path}/src/grpc_src-build
-  STAMP_DIR ${nih_cache_path}/src/grpc_src-stamp
-  TMP_DIR ${nih_cache_path}/tmp)
+  add_executable (gRPC::grpc_cpp_plugin IMPORTED)
+  exclude_if_included (gRPC::grpc_cpp_plugin)
 
-set (grpc_binary_dir "${grpc_src_BINARY_DIR}")
-set (grpc_source_dir "${grpc_src_SOURCE_DIR}")
+  if (grpc_FOUND AND NOT local_grpc)
+    # use installed grpc (via pkg-config)
+    macro (add_imported_grpc libname_)
+      if (static)
+        set (_search "${CMAKE_STATIC_LIBRARY_PREFIX}${libname_}${CMAKE_STATIC_LIBRARY_SUFFIX}")
+      else ()
+        set (_search "${CMAKE_SHARED_LIBRARY_PREFIX}${libname_}${CMAKE_SHARED_LIBRARY_SUFFIX}")
+      endif()
+      find_library(_found_${libname_}
+        NAMES ${_search}
+        HINTS ${grpc_LIBRARY_DIRS})
+      if (_found_${libname_})
+        message (STATUS "importing ${libname_} as ${_found_${libname_}}")
+      else ()
+        message (FATAL_ERROR "using pkg-config for grpc, can't find ${_search}")
+      endif ()
+      add_library ("gRPC::${libname_}" STATIC IMPORTED GLOBAL)
+      set_target_properties ("gRPC::${libname_}" PROPERTIES IMPORTED_LOCATION ${_found_${libname_}})
+      if (grpc_INCLUDE_DIRS)
+        set_target_properties ("gRPC::${libname_}" PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${grpc_INCLUDE_DIRS})
+      endif ()
+      target_link_libraries (ripple_libs INTERFACE "gRPC::${libname_}")
+      exclude_if_included ("gRPC::${libname_}")
+    endmacro ()
 
-set(CMAKE_CXX_COMPILER ${CMAKE_CXX_COMPILER})
-set(CMAKE_C_COMPILER ${CMAKE_C_COMPILER})
-set(gRPC_BUILD_CSHARP_EXT OFF CACHE BOOL "" FORCE)
-set(gRPC_INSTALL OFF CACHE BOOL "" FORCE)
-set(gRPC_BUILD_GRPC_CSHARP_PLUGIN OFF CACHE BOOL "" FORCE)
-set(gRPC_BUILD_GRPC_NODE_PLUGIN OFF CACHE BOOL "" FORCE)
-set(gRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN OFF CACHE BOOL "" FORCE)
-set(gRPC_BUILD_GRPC_PHP_PLUGIN OFF CACHE BOOL "" FORCE)
-set(gRPC_BUILD_GRPC_PYTHON_PLUGIN OFF CACHE BOOL "" FORCE)
-set(gRPC_BUILD_GRPC_RUBY_PLUGIN OFF CACHE BOOL "" FORCE)
-set(gRPC_CARES_PROVIDER "module" CACHE STRING "" FORCE)
-set(gRPC_SSL_PROVIDER "package" CACHE STRING "" FORCE)
+    set_target_properties (gRPC::grpc_cpp_plugin PROPERTIES
+        IMPORTED_LOCATION "${grpc_gpr_PREFIX}/bin/grpc_cpp_plugin${CMAKE_EXECUTABLE_SUFFIX}")
 
-find_package(ZLIB)
-if (ZLIB_FOUND)
-  set(gRPC_ZLIB_PROVIDER "package" CACHE STRING "" FORCE)
-else()
-  set(gRPC_ZLIB_PROVIDER "module" CACHE STRING "" FORCE)
+    pkg_check_modules (cares QUIET libcares)
+    if (cares_FOUND)
+      if (static)
+        set (_search "${CMAKE_STATIC_LIBRARY_PREFIX}cares${CMAKE_STATIC_LIBRARY_SUFFIX}")
+      else ()
+        set (_search "${CMAKE_SHARED_LIBRARY_PREFIX}cares${CMAKE_SHARED_LIBRARY_SUFFIX}")
+      endif()
+      find_library(_cares
+        NAMES ${_search}
+        HINTS ${cares_LIBRARY_DIRS})
+      if (NOT _cares)
+        message (FATAL_ERROR "using pkg-config for grpc, can't find c-ares")
+      endif ()
+      add_library (c-ares::cares STATIC IMPORTED GLOBAL)
+      set_target_properties (c-ares::cares PROPERTIES IMPORTED_LOCATION ${_cares})
+      if (cares_INCLUDE_DIRS)
+        set_target_properties (c-ares::cares PROPERTIES INTERFACE_INCLUDE_DIRECTORIES ${cares_INCLUDE_DIRS})
+      endif ()
+      exclude_if_included (c-ares::cares)
+    else ()
+      message (FATAL_ERROR "using pkg-config for grpc, can't find c-ares")
+    endif ()
+  else ()
+
+    FetchContent_GetProperties(grpc_src)
+    FetchContent_Populate(
+      grpc_src 
+      QUIET
+      GIT_REPOSITORY https://github.com/grpc/grpc.git
+      GIT_TAG v1.28.0
+      SOURCE_DIR ${nih_cache_path}/src/grpc_src
+      BINARY_DIR ${nih_cache_path}/src/grpc_src-build
+      STAMP_DIR ${nih_cache_path}/src/grpc_src-stamp
+      TMP_DIR ${nih_cache_path}/tmp)
+    
+    set (grpc_binary_dir "${grpc_src_BINARY_DIR}")
+    set (grpc_source_dir "${grpc_src_SOURCE_DIR}")
+    
+    # build options
+    set(CMAKE_CXX_COMPILER ${CMAKE_CXX_COMPILER})
+    set(CMAKE_C_COMPILER ${CMAKE_C_COMPILER})
+    set(gRPC_BUILD_CSHARP_EXT OFF CACHE BOOL "" FORCE)
+    set(gRPC_INSTALL OFF CACHE BOOL "" FORCE)
+    set(gRPC_BUILD_GRPC_CSHARP_PLUGIN OFF CACHE BOOL "" FORCE)
+    set(gRPC_BUILD_GRPC_NODE_PLUGIN OFF CACHE BOOL "" FORCE)
+    set(gRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN OFF CACHE BOOL "" FORCE)
+    set(gRPC_BUILD_GRPC_PHP_PLUGIN OFF CACHE BOOL "" FORCE)
+    set(gRPC_BUILD_GRPC_PYTHON_PLUGIN OFF CACHE BOOL "" FORCE)
+    set(gRPC_BUILD_GRPC_RUBY_PLUGIN OFF CACHE BOOL "" FORCE)
+    set(gRPC_CARES_PROVIDER "module" CACHE STRING "" FORCE)
+    set(gRPC_SSL_PROVIDER "package" CACHE STRING "" FORCE)
+    
+    find_package(ZLIB)
+    if (ZLIB_FOUND)
+      set(gRPC_ZLIB_PROVIDER "package" CACHE STRING "" FORCE)
+    else()
+      set(gRPC_ZLIB_PROVIDER "module" CACHE STRING "" FORCE)
+    endif()
+    
+    find_package(Protobuf 3.8)
+    if (Protobuf_FOUND)
+      set(gRPC_PROTOBUF_PROVIDER "package" CACHE STRING "" FORCE)
+    else()
+      set(gRPC_PROTOBUF_PROVIDER "module" CACHE STRING "" FORCE)
+    endif()
+    
+    # AbseilHelpers.cmake build type check checks _build_type for 
+    # "static". this conflicts with option static in 
+    # RippledSettings.cmake
+    set(static_bck ${static})
+    unset(static CACHE)
+    
+    add_subdirectory(${grpc_src_SOURCE_DIR} ${grpc_src_BINARY_DIR})
+    
+    # set the static back to the saved value
+    set(static ${static_bck} CACHE BOOL "link protobuf, openssl, libc++, and boost statically" FORCE)
+  endif()
 endif()
-
-find_package(Protobuf 3.8)
-if (Protobuf_FOUND)
-  set(gRPC_PROTOBUF_PROVIDER "package" CACHE STRING "" FORCE)
-else()
-  set(gRPC_PROTOBUF_PROVIDER "module" CACHE STRING "" FORCE)
-endif()
-
-#set(werr OFF CACHE BOOL "" FORCE)
-#set(CMAKE_CXX_FLAGS "-Wno-error=ignored-qualifiers -w -Wno-ignored-qualifiers")
-
-#add_definitions("-Wno-error=ignored-qualifiers -w -Wno-ignored-qualifiers")
-add_subdirectory(${grpc_src_SOURCE_DIR} ${grpc_src_BINARY_DIR})
-
-# set the static back to the saved value
-set(static ${static_bck} CACHE BOOL "link protobuf, openssl, libc++, and boost statically" FORCE)
 
 file (MAKE_DIRECTORY ${grpc_src_SOURCE_DIR}/include)
 
@@ -140,7 +211,6 @@ foreach(file ${GRPC_DEFINITION_FILES})
 endforeach()
 
 add_library (grpc_pbufs STATIC ${GRPC_PROTO_SRCS} ${GRPC_PROTO_HDRS})
-#target_include_directories (grpc_pbufs PRIVATE src)
 target_include_directories (grpc_pbufs SYSTEM PUBLIC ${GRPC_GEN_DIR})
 target_link_libraries (grpc_pbufs libprotobuf grpc++${grpc_suffix})
 target_compile_options (grpc_pbufs
