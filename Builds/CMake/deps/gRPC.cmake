99182df1c7916b7ec65cb90073fc42df0af90b3d
gregtatcam <gtsipenyuk@ripple.com>
2020-05-13 06:41:01 -0400

    Check if grpc is populated

--- Builds/CMake/deps/gRPC.cmake
+++ Builds/CMake/deps/gRPC.cmake
@@ -53,7 +53,7 @@ if (TARGET gRPC::gpr AND NOT local_grpc)
 else ()
   find_package (PkgConfig QUIET)
   if (PKG_CONFIG_FOUND)
-    pkg_check_modules (grpc QUIET "grpc${grpc_suffix}>=1.25" "grpc++${grpc_suffix}" gpr)
+      pkg_check_modules (grpc QUIET "grpc${grpc_suffix}>=1.25" "grpc++${grpc_suffix}" gpr)
   endif ()
 
   if (grpc_FOUND)
@@ -119,60 +119,80 @@ else ()
   else ()
 
     FetchContent_GetProperties(grpc_src)
-    FetchContent_Populate(
-      grpc_src 
-      QUIET
-      GIT_REPOSITORY https://github.com/grpc/grpc.git
-      GIT_TAG v1.28.0
-      SOURCE_DIR ${nih_cache_path}/src/grpc_src
-      BINARY_DIR ${nih_cache_path}/src/grpc_src-build
-      STAMP_DIR ${nih_cache_path}/src/grpc_src-stamp
-      TMP_DIR ${nih_cache_path}/tmp)
-    
-    set (grpc_binary_dir "${grpc_src_BINARY_DIR}")
-    set (grpc_source_dir "${grpc_src_SOURCE_DIR}")
-    
-    # build options
-    set(CMAKE_CXX_COMPILER ${CMAKE_CXX_COMPILER})
-    set(CMAKE_C_COMPILER ${CMAKE_C_COMPILER})
-    set(gRPC_BUILD_CSHARP_EXT OFF CACHE BOOL "" FORCE)
-    set(gRPC_INSTALL OFF CACHE BOOL "" FORCE)
-    set(gRPC_BUILD_GRPC_CSHARP_PLUGIN OFF CACHE BOOL "" FORCE)
-    set(gRPC_BUILD_GRPC_NODE_PLUGIN OFF CACHE BOOL "" FORCE)
-    set(gRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN OFF CACHE BOOL "" FORCE)
-    set(gRPC_BUILD_GRPC_PHP_PLUGIN OFF CACHE BOOL "" FORCE)
-    set(gRPC_BUILD_GRPC_PYTHON_PLUGIN OFF CACHE BOOL "" FORCE)
-    set(gRPC_BUILD_GRPC_RUBY_PLUGIN OFF CACHE BOOL "" FORCE)
-    set(gRPC_CARES_PROVIDER "module" CACHE STRING "" FORCE)
-    set(gRPC_SSL_PROVIDER "package" CACHE STRING "" FORCE)
-    
-    find_package(ZLIB)
-    if (ZLIB_FOUND)
-      set(gRPC_ZLIB_PROVIDER "package" CACHE STRING "" FORCE)
-    else()
-      set(gRPC_ZLIB_PROVIDER "module" CACHE STRING "" FORCE)
+
+    if (NOT grpc_src_POPULATED)
+      FetchContent_Populate(
+        grpc_src 
+        QUIET
+        GIT_REPOSITORY https://github.com/grpc/grpc.git
+        GIT_TAG v1.28.0
+        SOURCE_DIR ${nih_cache_path}/src/grpc_src
+        BINARY_DIR ${nih_cache_path}/src/grpc_src-build
+        STAMP_DIR ${nih_cache_path}/src/grpc_src-stamp
+        TMP_DIR ${nih_cache_path}/tmp)
+      
+      set (grpc_binary_dir "${grpc_src_BINARY_DIR}")
+      set (grpc_source_dir "${grpc_src_SOURCE_DIR}")
+      
+      # build options
+      set(CMAKE_CXX_COMPILER ${CMAKE_CXX_COMPILER})
+      set(CMAKE_C_COMPILER ${CMAKE_C_COMPILER})
+      set(gRPC_BUILD_CSHARP_EXT OFF CACHE BOOL "" FORCE)
+      set(gRPC_INSTALL OFF CACHE BOOL "" FORCE)
+      set(gRPC_BUILD_GRPC_CSHARP_PLUGIN OFF CACHE BOOL "" FORCE)
+      set(gRPC_BUILD_GRPC_NODE_PLUGIN OFF CACHE BOOL "" FORCE)
+      set(gRPC_BUILD_GRPC_OBJECTIVE_C_PLUGIN OFF CACHE BOOL "" FORCE)
+      set(gRPC_BUILD_GRPC_PHP_PLUGIN OFF CACHE BOOL "" FORCE)
+      set(gRPC_BUILD_GRPC_PYTHON_PLUGIN OFF CACHE BOOL "" FORCE)
+      set(gRPC_BUILD_GRPC_RUBY_PLUGIN OFF CACHE BOOL "" FORCE)
+      set(gRPC_CARES_PROVIDER "module" CACHE STRING "" FORCE)
+      set(gRPC_SSL_PROVIDER "package" CACHE STRING "" FORCE)
+      
+      find_package(ZLIB)
+      if (ZLIB_FOUND)
+        set(gRPC_ZLIB_PROVIDER "package" CACHE STRING "" FORCE)
+      else()
+        set(gRPC_ZLIB_PROVIDER "module" CACHE STRING "" FORCE)
+      endif()
+      
+      # AbseilHelpers.cmake build type check checks _build_type for 
+      # "static". this conflicts with option static in 
+      # RippledSettings.cmake
+      set(static_bck ${static})
+      unset(static CACHE)
+      
+      add_subdirectory(${grpc_src_SOURCE_DIR} ${grpc_src_BINARY_DIR})
+      
+      # set the static back to the saved value
+      set(static ${static_bck} CACHE BOOL "link protobuf, openssl, libc++, and boost statically" FORCE)
     endif()
     
-    # AbseilHelpers.cmake build type check checks _build_type for 
-    # "static". this conflicts with option static in 
-    # RippledSettings.cmake
-    set(static_bck ${static})
-    unset(static CACHE)
-    
-    add_subdirectory(${grpc_src_SOURCE_DIR} ${grpc_src_BINARY_DIR})
-    
-    # set the static back to the saved value
-    set(static ${static_bck} CACHE BOOL "link protobuf, openssl, libc++, and boost statically" FORCE)
+    macro (add_imported_grpc libname_)
+      add_library ("gRPC::${libname_}" STATIC IMPORTED GLOBAL)
+      set_target_properties ("gRPC::${libname_}" PROPERTIES
+        IMPORTED_LOCATION_DEBUG
+          ${grpc_binary_dir}/${ep_lib_prefix}${libname_}_d${ep_lib_suffix}
+        IMPORTED_LOCATION_RELEASE
+          ${grpc_binary_dir}/${ep_lib_prefix}${libname_}${ep_lib_suffix}
+        INTERFACE_INCLUDE_DIRECTORIES
+          ${grpc_source_dir}/include)
+      add_dependencies ("gRPC::${libname_}" grpc_src)
+      target_link_libraries (ripple_libs INTERFACE "gRPC::${libname_}")
+      exclude_if_included ("gRPC::${libname_}")
+    endmacro ()
 
+  
     if (${gRPC_PROTOBUF_PROVIDER} STREQUAL "module")
       add_executable (protobuf::protoc IMPORTED)
       set_target_properties (protobuf::protoc PROPERTIES
         IMPORTED_LOCATION $<TARGET_FILE:protoc>)
     endif()
   endif()
+  add_imported_grpc("grpc++${grpc_suffix}")
 endif()
 
-file (MAKE_DIRECTORY ${grpc_src_SOURCE_DIR}/include)
+
+#file (MAKE_DIRECTORY ${grpc_src_SOURCE_DIR}/include)
 
 set (GRPC_GEN_DIR "${CMAKE_BINARY_DIR}/proto_gen_grpc")
 file (MAKE_DIRECTORY ${GRPC_GEN_DIR})
@@ -233,7 +253,7 @@ foreach(file ${GRPC_DEFINITION_FILES})
     COMMAND protobuf::protoc
     ARGS --grpc_out=${GRPC_GEN_DIR}
          --cpp_out=${GRPC_GEN_DIR}
-         --plugin=protoc-gen-grpc=$<TARGET_FILE:grpc_cpp_plugin>
+         --plugin=protoc-gen-grpc=$<TARGET_FILE:gRPC::grpc_cpp_plugin>
          -I ${_proto_inc} -I ${_rel_dir}
          ${_abs_file}
     DEPENDS ${_abs_file}
@@ -247,7 +267,7 @@ endforeach()
 
 add_library (grpc_pbufs STATIC ${GRPC_PROTO_SRCS} ${GRPC_PROTO_HDRS})
 target_include_directories (grpc_pbufs SYSTEM PUBLIC ${GRPC_GEN_DIR})
-target_link_libraries (grpc_pbufs protobuf::libprotobuf grpc++${grpc_suffix})
+target_link_libraries (grpc_pbufs protobuf::libprotobuf "gRPC::grpc++${grpc_suffix}")
 target_compile_options (grpc_pbufs
   PRIVATE
     $<$<BOOL:${MSVC}>:-wd4065>
