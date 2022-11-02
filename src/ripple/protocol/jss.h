b56c83e772462d5d1153a424e1cbeb24392e9ff4
Gregory Tsipenyuk <gtsipenyuk@ripple.com>
2022-11-02 13:59:10 -0400

    Mainly name refactoring to be consistent with Amount/Amount2,Asset/Asset2.

--- src/ripple/protocol/jss.h
+++ src/ripple/protocol/jss.h
@@ -58,6 +58,8 @@ JSS(AMMWithdraw);            // transaction type
 JSS(Amendments);             // ledger type.
 JSS(Amount);                 // in: TransactionSign; field.
 JSS(Amount2);                // in/out: AMM IOU/XRP pool, deposit, withdraw amount
+JSS(Asset);                  // in: AMM Asset1
+JSS(Asset2);                 // in: AMM Asset2
 JSS(AuctionSlot);            // out: AMM Auction Slot
 JSS(AuthAccount);            // in: AMM Auction Slot
 JSS(AuthAccounts);           // in: AMM Auction Slot
@@ -126,8 +128,6 @@ JSS(TakerPays);              // field.
 JSS(Ticket);                 // ledger type.
 JSS(TicketCreate);           // transaction type.
 JSS(TimeInterval);           // out: AMM Auction Slot
-JSS(Asset);                  // in: AMM Asset1
-JSS(Asset2);                 // in: AMM Asset2
 JSS(TxnSignature);           // field.
 JSS(TradingFee);             // in/out: AMM trading fee
 JSS(TransactionType);        // in: TransactionSign.
@@ -169,6 +169,8 @@ JSS(api_version);            // in: many, out: Version
 JSS(api_version_low);        // out: Version
 JSS(applied);                // out: SubmitTransaction
 JSS(asks);                   // out: Subscribe
+JSS(asset);                  // int: AMM info
+JSS(asset2);                 // int: AMM info
 JSS(assets);                 // out: GatewayBalances
 JSS(authorized);             // out: AccountLines
 JSS(auth_change);            // out: AccountInfo
@@ -604,8 +606,6 @@ JSS(time);
 JSS(timeouts);                // out: InboundLedger
 JSS(track);                   // out: PeerImp
 JSS(traffic);                 // out: Overlay
-JSS(asset);                   // int: AMM info
-JSS(asset2);                  // int: AMM info
 JSS(total);                   // out: counters
 JSS(totalCoins);              // out: LedgerToJson
 JSS(total_bytes_recv);        // out: Peers
