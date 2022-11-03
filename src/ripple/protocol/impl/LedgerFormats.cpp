8305a0c845697339ed8ddc70e8091b92fb6014c2
Gregory Tsipenyuk <gtsipenyuk@ripple.com>
2022-11-03 12:23:01 -0400

    [FOLD] Replace AMMID with asset pair and other changes
    * Replace AMMID in all tx with asset pair Asset, Asset2
    * Add prefix to AMM AccountID calc
    * Calc AMM currency as 0x03 + 152 bits of sha256{cur1, cur2}
    * Rename sfTimeStamp to sfExpiration, all Amount type fields for the token pair to Amount and Amount2, Min/MaxSlotPrice to Min/MaxBidPrice.
    * Update auction slot price for slot 0
    * Nest the “AMM” object’s fields inside a new top-level response field "amm"
    * Add required subtx flags for AMMDeposit/Withdraw
    * Charge transfer fee on Create/Deposit
    * Remove transfer fee on Payment
    * Move sfAsset/sfAsset2 in AMM object up from top-level sfAMMToken
    * Change single token asset deposit to limit
    * Add user input validation on Deposit/Withdraw
    * Address round-off on Withdraw

--- src/ripple/protocol/impl/LedgerFormats.cpp
+++ src/ripple/protocol/impl/LedgerFormats.cpp
@@ -275,7 +275,8 @@ LedgerFormats::LedgerFormats()
             {sfVoteSlots, soeOPTIONAL},
             {sfAuctionSlot, soeOPTIONAL},
             {sfLPTokenBalance, soeREQUIRED},
-            {sfAMMToken, soeREQUIRED}
+            {sfAsset, soeREQUIRED},
+            {sfAsset2, soeREQUIRED}
         },
         commonFields);
 
