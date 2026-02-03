9df5cbc489fe5a94380da2fa3184613bfb6b558e
Gregory Tsipenyuk <gregtatcam@users.noreply.github.com>
2026-02-03 11:27:11 -0500

    Fix ledger_entry for AMM with MPT (#58)

--- src/xrpld/rpc/handlers/LedgerEntry.cpp
+++ src/xrpld/rpc/handlers/LedgerEntry.cpp
@@ -116,12 +116,12 @@ parseAMM(
         return Unexpected(value.error());
     }
 
-    auto const asset = LedgerEntryHelpers::requiredIssue(
+    auto const asset = LedgerEntryHelpers::requiredAsset(
         params, jss::asset, "malformedRequest");
     if (!asset)
         return Unexpected(asset.error());
 
-    auto const asset2 = LedgerEntryHelpers::requiredIssue(
+    auto const asset2 = LedgerEntryHelpers::requiredAsset(
         params, jss::asset2, "malformedRequest");
     if (!asset2)
         return Unexpected(asset2.error());
