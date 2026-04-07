0156cfde6120f32da42f014b302074f97a7cdd24
Gregory Tsipenyuk <gtsipenyuk@ripple.com>
2026-04-07 15:59:32 -0400

    Fix double adjustOwnerCount() in AMMWithdraw.

--- src/libxrpl/tx/transactors/dex/AMMWithdraw.cpp
+++ src/libxrpl/tx/transactors/dex/AMMWithdraw.cpp
@@ -607,10 +607,6 @@ AMMWithdraw::withdraw(
             auto const balance_ = isIssue ? std::max(priorBalance, balance.xrp()) : priorBalance;
             if (balance_ < reserve)
                 return tecINSUFFICIENT_RESERVE;
-
-            // Update owner count.
-            if (!isIssue)
-                adjustOwnerCount(view, sleAccount, 1, journal);
         }
         return tesSUCCESS;
     };
